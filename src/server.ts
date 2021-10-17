00563766d1ef71bdb52623b485db7891b80a32af
ctnkaan <kaan.taskingenc@gmail.com>
2021-10-18 00:30:24 +0300

    added basic scam detection

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,33 @@
 //<> with ❤️ by Postman Student Leaders
 
 import dotenv from 'dotenv';
+const { Client, Intents } = require("discord.js");
+const Bot = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 dotenv.config();
 
-const { Client, Intents } = require("discord.js");
-const Bot = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-});
+
+const ScamDetector = require("./commands/scamLinkDetector")
+
+const prefix = "!helper" 
+
+
+
+
 
 Bot.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
+    Bot.user.setActivity("<> with ❤️ by Postman Student Leaders");
 });
 
-Bot.on("messageCreate", (message: { content: string; channel: { send: (arg0: string) => void; }; }) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+Bot.on("messageCreate", (message: any) => {
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    ScamDetector.run(message);
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
 });
 
 

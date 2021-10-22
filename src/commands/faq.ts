f1381b86d816036ea6aef6f3d3f3927d2e7aad2a
ctnkaan <kaan.taskingenc@gmail.com>
2021-10-22 22:01:23 +0300

    added FAQ template

---
+++ src/commands/faq.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "faq",
+    run(message: any) {
+        message.channel.send("FAQ");
+    },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
 //<> with ❤️ by Postman Student Leaders
 
 import dotenv from 'dotenv';
+dotenv.config();
 const { Client, Intents } = require("discord.js");
 const Bot = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-dotenv.config();
+const fs = require('fs');
 
 
-const ScamDetector = require("./commands/scamLinkDetector")
+const ScamDetector = require('./commands/scamLinkDetector');
+const Faq = require('./commands/faq');
 
-const prefix = "!helper" 
-
-
-
-
+//Prefix
+const prefix = "!p" 
 
 Bot.on("ready", () => {
     console.log("I am ready!");
@@ -21,12 +20,21 @@ Bot.on("ready", () => {
 
 Bot.on("messageCreate", (message: any) => {
 
+    ScamDetector.run(message);
+
+    if ((!message.content.startsWith(prefix)) || message.author.bot) return;
+
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    if (message.author.bot) return;
-    ScamDetector.run(message);
+    switch (command) {
+        case "faq":
+            Faq.run(message);
+            break;
 
+        default:
+            message.channel.send("Command not found");
+    }
 });
 
 

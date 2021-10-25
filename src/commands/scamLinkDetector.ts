c8864b2a0b4173c462ceae48691da906ce2bcf9e
ibrahim halil sakli <63147096+softwareVirus@users.noreply.github.com>
2021-10-25 11:17:51 +0300

    Update scamLinkDetector.ts
    "I changed the control statement for a lot of suspicious sentences. I hope it will help check more sus. sentences. (the change is in ./src/commands/scamLinkDetectors.tx)"

--- src/commands/scamLinkDetector.ts
+++ src/commands/scamLinkDetector.ts
@@ -2,8 +2,9 @@ module.exports = {
     name: "scamLinkDetector",
     description: "Detects scam links",
     run: (msg: any) => {
-        
-        if (msg.content.toLowerCase().includes("nitro") || msg.content.toLowerCase().includes("i leave from cs:go")) {
+        var suspicious_words:string[];
+        suspicious_words = ["nitro","i leave from cs:go","grafiby.link"];
+        if (suspicious_words.indexOf(msg.content.toLowerCase()) !== -1) {
             msg.delete();
             msg.channel.send(`A suspicious message has been deleted!`);
         }

fedf10891cea2bdf46c5a0ab7a1493a1dfd23160
ctnkaan <kaan.taskingenc@gmail.com>
2022-01-18 03:47:26 +0300

    added mongoDB database

--- src/commands/scamLinkDetector.ts
+++ src/commands/scamLinkDetector.ts
@@ -1,4 +1,8 @@
 import { isSuspiciousLink } from "../util/isSuspiciousLink";
+import Schema from "../database/schema";
+
+
+let isScamLink :boolean = false;
 
 export = {
     name: "scamLinkDetector",
@@ -14,7 +18,9 @@ export = {
                     "Word nitro is banned due to increase in scams. If you see multiple of these messages your account is probably infected."
                 )
                 .catch(console.error);
-            msg.delete();
+            msg.delete().catch(console.error);
+            isScamLink = true;
+            
         } else {
             /** Check for suspicious link in message */
             const links = msg.content.match(/(https?:\/\/\S+)\b/g);
@@ -46,10 +52,25 @@ export = {
                         )} \n\nIf you didn't intentionally send these links, there is a change your account was hacked and is sending out malicious messages. In this case, **please change your password and enable two-factor authentication on your Discord account**.\n\nIf you do not take remedial action we will need to ban your account from the Postman Student Server. Contact a Postmanaut if you have any questions or you believe this was an error!`;
 
                         msg.author.send(dmMsg).catch(console.error);
-                        msg.delete();
+                        msg.delete().catch(console.error);
+                        isScamLink = true;
                     }
                 }
             }
+        }
+
+        if (isScamLink) {
+            try {
+                setTimeout(async () => {
+                    await new Schema({
+                        id_username: msg.author.id + " - "+ msg.author.username,
+                        message: msg.content
+                    }).save();
+                }, 1000);
+            } catch (error) {
+                console.log(error);
+            }
+
         }
     }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 //<> with ❤️ by Postman Student Leaders
 
 import Discord from "discord.js";
+import mongoose from 'mongoose';
 
 //commands
 import ScamDetector from "./commands/scamLinkDetector";
@@ -18,8 +19,12 @@ const client = new Discord.Client();
 //Prefix
 const prefix = "!p";
 
-client.on("ready", () => {
+client.on("ready", async () => {
     if (!client.user) return; // to appease typescript. In reality, this will never happen
+    await mongoose.connect(process.env.MONGO_URI!, {
+        keepAlive: true,
+    });
+
     console.log(`I am ready! Logged in as ${client.user.tag}`);
     client.user.setActivity(`${prefix} help`);
 });

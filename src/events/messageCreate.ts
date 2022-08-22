2a734caf7d81fbe33a3f4b80602a459eb2204a60
ctnkaan <kaan.taskingenc@gmail.com>
2022-08-22 18:50:52 +0300

    update messageCreate event

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,29 @@
+export = {
+  execute(message, commands, silentCommands, prefix ) {
+    //Ignore bot messages
+    if (message.author.bot) return;
+
+    //Runs for every message
+    silentCommands.get("scam detector").callback(message);
+    silentCommands.get("gnt detector").callback(message);
+
+    //If the message does not start with the prefix return
+    if (!message.content.startsWith(prefix)) return;
+
+    //get the message content without the prefix
+    const args: string[] = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
+
+    //Check if the command exists in the hashmap. It returns undefined if it doesn't exist
+    const inputedCommand = commands.get(args.shift()!.toLowerCase());
+    
+
+    //If the currCommand is not undefined,
+    if (inputedCommand) 
+        inputedCommand.callback(message, args);
+    else if (inputedCommand === null || inputedCommand === undefined)
+        message.channel.send(`Command not found! Type ${prefix} help to see all commands`);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ import config from "./config";
 
 //events
 import Ready from "./events/ready";
-import CommandMap from "./events/commandMap"
+import CommandMap from "./events/commandMap";
+import MessageCreate from "./events/messageCreate";
 
 //types
 import { MessageType } from "./types/message";
@@ -35,35 +36,7 @@ bot.on("ready", async () => {
 
 //When there is a message in server
 bot.on("messageCreate", async (message: MessageType) => {
-
-    //Ignore bot messages
-    if (message.author.bot) return;
-
-    //Runs for every message
-    silentCommands.get("scam detector").callback(message);
-    silentCommands.get("gnt detector").callback(message);
-
-    //If the message does not start with the prefix return
-    if (!message.content.startsWith(config.prefix)) return;
-
-    //get the message content without the prefix
-    const args: string[] = message.content
-        .slice(config.prefix.length)
-        .trim()
-        .split(/ +/g);
-
-    //get the command
-    const command: string = args.shift()!.toLowerCase();
-
-    //Check if the command exists in the hashmap. It returns undefined if it doesn't exist
-    const currCommand = commands.get(command);
-    
-
-    //If the currCommand is not undefined,
-    if (currCommand) 
-        currCommand.callback(message, args);
-    else
-        message.channel.send(`Command not found! Type ${config.prefix} help to see all commands`);
+  MessageCreate.execute(message, commands, silentCommands, config.prefix);
 });
 
 bot.login(process.env.DISCORD_TOKEN);
